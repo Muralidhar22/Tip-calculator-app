7087ee334dcc3925b4931957749fd11c352ccc3b
Muralidhar22 <muralidhar7794@gmail.com>
2022-01-26 20:37:03 +0530

    first commit

---
+++ tip-calculator-app.js
@@ -0,0 +1,6 @@
+const tipPercentage = document.querySelectorAll(".button-percent-formatter")
+const bill = document.querySelector(".bill")
+
+bill.addEventListener('click',() => {
+    bill.style.borderColor = "hsl(172, 67%, 45%)";
+})
