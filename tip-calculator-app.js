c1f9af6c983ca84d720ea0acf65579cdc0aca920
Muralidhar22 <muralidhar7794@gmail.com>
2022-01-27 18:06:46 +0530

    2nd commit

--- tip-calculator-app.js
+++ tip-calculator-app.js
@@ -1,6 +1,15 @@
 const tipPercentage = document.querySelectorAll(".button-percent-formatter")
 const bill = document.querySelector(".bill")
+const reset = document.querySelector(".reset-button")
 
-bill.addEventListener('click',() => {
-    bill.style.borderColor = "hsl(172, 67%, 45%)";
+bill.addEventListener('input',() =>{
+    console.log(bill.value)
+    if(bill.value === '0'){
+        console.log("0 bruhhh")
+       
+    }
+})
+
+reset.addEventListener('click',()=>{
+    reset.classList.remove("reset-btn-mute")
 })
