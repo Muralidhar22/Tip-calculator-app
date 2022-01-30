9b6100d1207390e7e89af27edb96b80d0e7b7984
Muralidhar22 <muralidhar7794@gmail.com>
2022-01-30 17:26:25 +0530

    Js Working V2, tip buttons fixed

--- tip-calculator-app.js
+++ tip-calculator-app.js
@@ -1,39 +1,37 @@
-let tipPercentage = document.querySelectorAll(".button-percent-formatter")
 const bill = document.querySelector(".bill")
 const reset = document.querySelector(".reset-button")
-const inputField = [
-    document.querySelector('.people'),
-    document.querySelector('.bill'),
-    document.querySelector('.custom-percent')
-]
+const zeroBillError = document.querySelector(".bill-span")
+const zeroPeopleError = document.querySelector(".people-span")
+const inputField = document.querySelectorAll("input[type='text']")
 const people = document.querySelector('.people')
-let clickedBtnValue
-let billValue
-let noOfPerson
-let customPercent
 const customTipPercent = document.querySelector(".custom-percent")
 const tipAmountPerPerson = document.querySelector(".tip-amount-number")
 const totalAmountPerPerson = document.querySelector(".total-number")
 
+let tipPercent, billValue ,noOfPerson, customPercent  
+let tipContainer = document.querySelectorAll(".button-percent-formatter")
 
 //bill
 bill.addEventListener('input',() =>{
     reset.classList.add("reset-btn-mute")
     billValue = parseFloat(bill.value)
-    if(bill.value === '0'){
-        console.log("Nah bruhhh")
+    tipCalculator()
+    if(billValue === 0.00){
+        errorAdd(zeroBillError)
     }
 })
 
 // tip percentage button active
-tipPercentage.forEach(item =>{
+tipContainer.forEach(item =>{
     item.addEventListener('click', () => {
-        clickedBtnValue = item.value
+        tipPercent = item.value
         //adding button active to only the clicked button
-         tipPercentage.forEach(button =>{
-             if(button.value === clickedBtnValue){
+         tipContainer.forEach(button =>{
+             if(button.value === tipPercent){
                     button.classList.add("button-active");
-                    tipCalculator(billValue,clickedBtnValue,noOfPerson)
+                    if(people.value !== 0 && people.value !== ''){
+                    tipCalculator()
+                }
              }
              else{
                  button.classList.remove("button-active");
@@ -47,69 +45,82 @@ customTipPercent.addEventListener('input',()=>{
     if(customTipPercent === document.activeElement){
         btnInactive()
         customPercent = parseInt(customTipPercent.value)
-        tipCalculator(billValue,customPercent,noOfPerson)
+        if(customPercent !== '' && customPercent !== 0){
+            tipPercent = 0
+            tipCalculator()
+        } 
     }
 })
 
 // no of person
 people.addEventListener('input',() =>{
-   noOfPerson = people.value
-       if(clickedBtnValue){
-            tipCalculator(billValue,clickedBtnValue,noOfPerson)
+    noOfPerson = parseInt(people.value)
+    if(peopleValidator()) { 
+        if(noOfPerson === 0){
+            errorAdd(zeroPeopleError)
+        }
+         return
+    }
+    else if(tipPercent){
+            tipCalculator()
        }
-        else{
-            if(isNaN(customTipPercent.value) ){
-                return
-           }
-           else{  tipCalculator(billValue,customPercent,noOfPerson) }
+    else{
+            if(isNaN(customPercent) || customPercent === '') return
+            else tipCalculator()  
         }   
 })
-
 //TIP CALCULATION FUNCTION
-function tipCalculator(bill,tip,people){
-    let sum 
-    let tipPerPerson 
-    let totalPerPerson 
-  
-    if(isNaN(people) || people === '' || people === 0) { 
-        makeZero()
-    return
+function tipCalculator(){  
+         if(peopleValidator()) return
+         else{
+            errorRemove(zeroBillError,zeroPeopleError)
+            if(tipPercent === 0) percentCalculator(customPercent)
+            else percentCalculator(tipPercent) 
+        }  
 }
-    else{
-        console.log(`tip bill : ${tip/100}`)
-        sum = (bill + (bill*(tip/100)) )
-        totalPerPerson = (sum/people).toFixed(2)
-        tipPerPerson = ((bill*(tip/100))/people).toFixed(2);
-        tipAmountPerPerson.textContent = `$${tipPerPerson}`
-        totalAmountPerPerson.textContent = `$${totalPerPerson}`
-    }    
+function percentCalculator(percent){
+    let sum, tipPerPerson, totalPerPerson
+    sum = (billValue + (billValue*(percent/100)) )
+    totalPerPerson = (sum/noOfPerson).toFixed(2)
+    tipPerPerson = ((billValue*(percent/100))/noOfPerson).toFixed(2);
+    tipAmountPerPerson.textContent = `$${tipPerPerson}`
+    totalAmountPerPerson.textContent = `$${totalPerPerson}`
 }
-
 // reset button handler
 reset.addEventListener('click',()=>{
     reset.classList.remove("reset-btn-mute")
     //making tip percentage button inactive
     btnInactive()
-    
+    errorRemove(zeroBillError,zeroPeopleError)
     inputField.forEach((element)=>{
         element.value = ''
     })
-
     makeZero()
-
 })
-
 //makeZero function
 const makeZero = () =>{
-    tipAmountPerPerson.textContent = `$0.00`
-    totalAmountPerPerson.textContent = `$0.00`
+    tipAmountPerPerson.textContent = totalAmountPerPerson.textContent = `$0.00`
+    tipPercent = billValue = noOfPerson = customPercent = 0
 }
-
 //make button inactive
-
 const btnInactive = () => {
-    tipPercentage.forEach(item =>{
+    tipContainer.forEach(item =>{
         item.classList.remove("button-active")
     })
 }
-
+// error add and remove functions
+const errorAdd = (...element) =>{  
+    element.forEach(item =>{
+        item.classList.add("label-span-active")
+    })
+    
+}
+const errorRemove = (...element) => {
+    element.forEach(item =>{
+        item.classList.remove("label-span-active")
+    })
+}
+const peopleValidator = () =>{
+    if(noOfPerson === '' || noOfPerson === 0 || isNaN(noOfPerson)) return true
+    else return false
+}
